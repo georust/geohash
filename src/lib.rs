5dbd3a275ab032f8ee5254253f3f340862ecb2a0
Sun Ning <sunng@about.me>
2015-01-10 13:01:44 +0800

    rustup to 1.0 alpha

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
+#![allow(unstable)]
 #![crate_name = "geohash"]
 #![crate_type = "rlib"]
-
-#[cfg(test)]
-use std::num::FloatMath;
 
 static BASE32_CODES: &'static [char] =
     &['0', '1', '2', '3', '4', '5', '6', '7',
@@ -19,12 +17,12 @@ static BASE32_CODES: &'static [char] =
 ///
 /// Returns:
 /// Geohash encoded `String`
-pub fn encode(lat: f32, lon: f32, num_chars: uint) -> String {
+pub fn encode(lat: f32, lon: f32, num_chars: usize) -> String {
     let mut out: String = String::new();
 
     let mut bits: i8 = 0;
     let mut bits_total: i8 = 0;
-    let mut hash_value: uint = 0;
+    let mut hash_value: usize = 0;
     let mut max_lat = 90f32;
     let mut min_lat = -90f32;
     let mut max_lon = 180f32;
@@ -35,7 +33,7 @@ pub fn encode(lat: f32, lon: f32, num_chars: uint) -> String {
         if  bits_total % 2 == 0 {
             mid = (max_lon + min_lon) / 2f32;
             if lon > mid {
-                hash_value = (hash_value << 1) + 1u;
+                hash_value = (hash_value << 1) + 1us;
                 min_lon = mid;
             } else {
                 hash_value = hash_value << 1;
@@ -44,7 +42,7 @@ pub fn encode(lat: f32, lon: f32, num_chars: uint) -> String {
         } else {
             mid = (max_lat + min_lat) / 2f32;
             if lat > mid {
-                hash_value = (hash_value << 1) + 1u;
+                hash_value = (hash_value << 1) + 1us;
                 min_lat = mid;
             } else {
                 hash_value = hash_value << 1;
@@ -66,11 +64,11 @@ pub fn encode(lat: f32, lon: f32, num_chars: uint) -> String {
 }
 
 trait Indexable<T: Eq> {
-    fn index_of(&self, item:T) -> uint;
+    fn index_of(&self, item:T) -> usize;
 }
 
 impl<'a, T: Eq> Indexable<T> for &'a [T] {
-    fn index_of(&self, item:T) -> uint {
+    fn index_of(&self, item:T) -> usize {
         for c in range(0, self.len()) {
             if item == self[c] {
                 return c
@@ -98,14 +96,14 @@ pub fn decode_bbox(hash_str: &str) -> (f32, f32, f32, f32){
     let mut max_lon = 180f32;
     let mut min_lon = -180f32;
     let mut mid: f32;
-    let mut hash_value: uint;
+    let mut hash_value: usize;
 
     let chars: Vec<char> = hash_str.chars().collect();
     for c in chars.into_iter() {
         hash_value = BASE32_CODES.index_of(c);
 
         for bs in range(0, 5) {
-            let bit = (hash_value >> (4 - bs)) & 1u;
+            let bit = (hash_value >> (4 - bs)) & 1us;
             if is_lon {
                 mid = (max_lon + min_lon) / 2f32;
 
@@ -146,17 +144,23 @@ pub fn decode(hash_str: &str) -> (f32, f32, f32, f32) {
     ((lat1+lat0)/2f32, (lon1+lon0)/2f32, (lat1-lat0)/2f32, (lon1-lon0)/2f32)
 }
 
-#[test]
-fn test_encode() {
-    assert_eq!(encode(37.8324f32, 112.5584f32, 9u), "ww8p1r4t8".to_string());
-    assert_eq!(encode(32f32, 117f32, 3u), "wte".to_string());
-}
+#[cfg(test)]
+mod test {
+    use ::{encode, decode, decode_bbox};
+    use std::num::Float;
 
-#[test]
-fn test_decode() {
-    assert_eq!(decode_bbox("ww8p1r4t8"),
-               (37.832367f32, 37.832409f32, 112.558365f32, 112.558411f32));
-    let (lat, lon, _, _) = decode("ww8p1r4t8");
-    assert_eq!(FloatMath::abs_sub(lat, 37.8324f32) < 1e-4f32, true);
-    assert_eq!(FloatMath::abs_sub(lon, 112.5584f32) < 1e-4f32, true);
+    #[test]
+    fn test_encode() {
+        assert_eq!(encode(37.8324f32, 112.5584f32, 9us), "ww8p1r4t8".to_string());
+        assert_eq!(encode(32f32, 117f32, 3us), "wte".to_string());
+    }
+
+    #[test]
+    fn test_decode() {
+        assert_eq!(decode_bbox("ww8p1r4t8"),
+                   (37.832367f32, 37.832409f32, 112.558365f32, 112.558411f32));
+        let (lat, lon, _, _) = decode("ww8p1r4t8");
+        assert_eq!(Float::abs_sub(lat, 37.8324f32) < 1e-4f32, true);
+        assert_eq!(Float::abs_sub(lon, 112.5584f32) < 1e-4f32, true);
+    }
 }
