44023600efc507093b57009a82e9c7e9e9dd45c7
uima <git.m5svm@uimataso.com>
2025-06-05 15:43:09 +0800

    significand is not a typo

--- src/core.rs
+++ src/core.rs
@@ -133,7 +133,7 @@ pub fn encode(c: Coord<f64>, len: usize) -> Result<String, GeohashError> {
     }
 
     // divides the latitude by 180, then adds 1.5 to give a value between 1 and 2
-    // then we take the first 32 bits of the significant as a u32
+    // then we take the first 32 bits of the significand as a u32
     let lat32 = ((c.y * 0.005555555555555556 + 1.5).to_bits() >> 20) as u32;
     // same as latitude, but a division by 360 instead of 180
     let lon32 = ((c.x * 0.002777777777777778 + 1.5).to_bits() >> 20) as u32;
